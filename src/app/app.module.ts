89ed5b5ad037d730154a301c00f64ba2e95fb455
Josh M. Diaz <joshua.michael.diaz@gmail.com>
2018-01-25 21:43:02 -0700

    Getting authentication pieces started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,51 +1,47 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { BrowserModule } from "@angular/platform-browser";
+import { ErrorHandler, NgModule } from "@angular/core";
+import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
 
 //*********** ionic Native **************/
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { Geolocation } from '@ionic-native/geolocation';
+import { StatusBar } from "@ionic-native/status-bar";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { Geolocation } from "@ionic-native/geolocation";
 
-import { MyApp } from './app.component';
+import { MyApp } from "./app.component";
 
 //***********  Angularfire2 v5 **************/
 
-import { AngularFireModule } from 'angularfire2';
+import { AngularFireModule } from "angularfire2";
 // New imports to update based on AngularFire2 version 4
-import { AngularFireDatabaseModule } from 'angularfire2/database-deprecated';
-import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from "angularfire2/database-deprecated";
+import { AngularFireAuthModule } from "angularfire2/auth";
 
 //***********  Facebook **************/
-import { Facebook } from '@ionic-native/facebook';
+import { Facebook } from "@ionic-native/facebook";
 //***********  Google plus **************/
-import { GooglePlus } from '@ionic-native/google-plus';
+import { GooglePlus } from "@ionic-native/google-plus";
 
 //*********** Provider **************/
-import { AuthData } from '../providers/auth-data';
-import { RadioPlayer } from '../providers/radio-service';
-
+import { AuthData } from "../providers/auth-data";
+import { RadioPlayer } from "../providers/radio-service";
 
 //************** import image gallery *********************//
 
-import * as ionicGalleryModal from 'ionic-gallery-modal';
-import { HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
-
+import * as ionicGalleryModal from "ionic-gallery-modal";
+import { HAMMER_GESTURE_CONFIG } from "@angular/platform-browser";
 
 //********** firebase configuration  ************ */
 export const firebaseConfig = {
-  apiKey: 'your-key',
-  authDomain: 'your-project-authdomain',
-  databaseURL: 'your-database-URL',
-  projectId: 'your-project-id',
-  storageBucket: 'your-storage-bucket',
-  messagingSenderId: 'your-messaging-sender-id'
+  apiKey: "AIzaSyAMA0D9NvHCYjB-JMXZWGW0RMQpUgyGERY",
+  authDomain: "barker-503ee.firebaseapp.com",
+  databaseURL: "https://barker-503ee.firebaseio.com",
+  projectId: "barker-503ee",
+  storageBucket: "barker-503ee.appspot.com",
+  messagingSenderId: "527910470763"
 };
 
 @NgModule({
-  declarations: [
-    MyApp
-  ],
+  declarations: [MyApp],
   imports: [
     BrowserModule,
     ionicGalleryModal.GalleryModalModule,
@@ -56,18 +52,16 @@ export const firebaseConfig = {
     AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp
-  ],
+  entryComponents: [MyApp],
   providers: [
     StatusBar,
     SplashScreen,
     Geolocation,
     {
       provide: HAMMER_GESTURE_CONFIG,
-      useClass: ionicGalleryModal.GalleryModalHammerConfig,
+      useClass: ionicGalleryModal.GalleryModalHammerConfig
     },
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
     AuthData,
     Facebook,
     RadioPlayer,
