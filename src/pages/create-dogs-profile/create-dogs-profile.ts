a7d991daf3d44a5732da7a5649a7eaf6cd23f098
Josh Diaz <josh@iserveproducts.com>
2018-02-07 20:38:58 -0700

    Working on dog profile

--- src/pages/create-dogs-profile/create-dogs-profile.ts
+++ src/pages/create-dogs-profile/create-dogs-profile.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { ProfileModel } from "../../models/profile";
+import { DogModel } from "../../models/dog";
+import { AngularFireAuth } from "angularfire2/auth";
 
 /**
  * Generated class for the CreateDogsProfilePage page.
@@ -10,16 +13,44 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-create-dogs-profile',
-  templateUrl: 'create-dogs-profile.html',
+  selector: "page-create-dogs-profile",
+  templateUrl: "create-dogs-profile.html"
 })
 export class CreateDogsProfilePage {
+  profile = {} as ProfileModel;
+  dog = {
+    name: "",
+    breed: "",
+    gender: "",
+    eyes: "",
+    fixed: false,
+    papered: false,
+    registered: false,
+    description: "",
+    birthdate: "",
+    photos: []
+  } as DogModel;
+  dogs: Array<{}>;
+  uid: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public afAuth: AngularFireAuth) {}
+
+  addEmptyDogs(num) {
+    this.dogs = [];
+    for (let i = 0; i < num; i++) {
+      this.dogs.push(this.dog);
+    }
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad CreateDogsProfilePage');
+    if (this.navParams.data && this.navParams.data.profileData) {
+      this.profile = this.navParams.data.profileData;
+      console.log(this.profile);
+    }
+    this.afAuth.authState.subscribe(userAuth => {
+      if (userAuth) {
+        this.uid = userAuth.uid;
+      }
+    });
   }
-
 }
