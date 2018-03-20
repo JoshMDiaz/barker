05cb6f1899df3fa46ba0cfaacfa0907746718bf8
Josh Diaz <josh@iserveproducts.com>
2018-03-19 19:55:55 -0600

    Building out filter

--- src/pages/dog-search-modal/dog-search-modal.ts
+++ src/pages/dog-search-modal/dog-search-modal.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import "rxjs/add/operator/map";
 
 @IonicPage()
 @Component({
@@ -8,8 +10,19 @@ import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angul
 })
 export class DogSearchModalPage {
   dogs: Array<any>;
+  breeds: Array<string>;
+  eyeColors: Array<string>;
+  filter: {
+    breed: string,
+    eyes: string,
+    gender?: string
+  };
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private viewCtrl: ViewController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private viewCtrl: ViewController, private http: Http) {
+    this.filter = {
+      breed: '',
+      eyes: '',
+    };
   }
 
   close() {
@@ -17,8 +30,26 @@ export class DogSearchModalPage {
     this.viewCtrl.dismiss(/*data*/);
   }
 
+  filterDogs(filter) {
+    console.log(filter);
+
+  }
+
   ionViewDidLoad() {
     this.dogs = this.navParams.data.dogs;
+    this.http
+      .get("/assets/data/breeds.json")
+      .map(data => data.json())
+      .subscribe(data => {
+        this.breeds = data;
+      });
+
+    this.http
+      .get("/assets/data/eye-colors.json")
+      .map(data => data.json())
+      .subscribe(data => {
+        this.eyeColors = data;
+      });
   }
 
 }
